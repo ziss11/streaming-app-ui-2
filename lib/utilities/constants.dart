609db9911c9ef2e965a4511b90d8fe755c7c802f
Abdul Azis <83083246+Aziszy@users.noreply.github.com>
2022-03-04 21:18:20 +0700

    add constants file
    This file containing constants variables expcept colors variables

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+double defaultMargin = 30;
+double defaultRadius = 12;
+
+const light = FontWeight.w300;
+const regular = FontWeight.w400;
+const medium = FontWeight.w500;
