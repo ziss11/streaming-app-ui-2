f10a454cda2d341aaaec84d6125ed0dbfd482ab8
Abdul Azis <83083246+Aziszy@users.noreply.github.com>
2022-03-04 21:02:21 +0700

    add colors file
    This colors file containing constant colors variable

---
+++ android/app/src/main/kotlin/com/example/streaming_app_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.streaming_app_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utilities/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+const backgroundColor = Color(0xff1D2027);
+const primaryColor = Color(0xff546EE5);
+const whiteColor = Color(0xffFFFFFF);
+const orangeColor = Color(0xffFFA235);
+const greyColor = Color(0xff696D74);
+const ligthGreyColor = Color(0xffB2B5BB);
+const pinkColor = Color(0xffFF7474);
+const blackColor = Color(0xff252932);
